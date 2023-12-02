00c850005275e02ca34105880a7d16e89993519c
Priyanshu Dhyani <priyanshudhyani2002@gmail.com>
2023-12-02 09:59:18 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accountId = 15102002
+let accountEmail = "priyanshu@google.com"
+var accountPassword = 263667
+accountCity = "Jaipur"
+let accountState;
+
+
+accountEmail = "pqrs@gmail.com"
+accountPassword = 783039
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
